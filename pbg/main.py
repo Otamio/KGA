ebc3556e3bca069b34606f8878defe01cc7d849a
Otamio <ca.john.wang@gmail.com>
2022-01-05 23:21:17 -0800

    add entry to pytorch-biggraph

---
+++ pbg/main.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE.txt file in the root directory of this source tree.
+
+import argparse
+import logging
+from pathlib import Path
+from datetime import datetime
+
+import torch
+import attr
+from torchbiggraph.config import parse_config
+from torchbiggraph.converters.importers import TSVEdgelistReader, convert_input_data
+from torchbiggraph.eval import do_eval
+from torchbiggraph.filtered_eval import FilteredRankingEvaluator
+from torchbiggraph.train import train
+from torchbiggraph.util import (
+    SubprocessInitializer,
+    set_logging_verbosity,
+    setup_logging,
+)
+
+import os
+
+# remove the Issue: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+os.environ['MKL_THREADING_LAYER'] = 'GNU'
+
+# Model mapping
+op_dict = {'complex': "complex_diagonal",
+           'distmult': "diagonal",
+           'transe': "translation"}
+
+comp_dict = {'complex': "dot",
+             'distmult': "dot",
+             'transe': "l2"}
+
+
+def try_to_make_dir(folder):
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+
+######
+# Read the parameters
+######
+
+
+parser = argparse.ArgumentParser(
+    description="Running PyTorch-BigGraph"
+)
+parser.add_argument('--dataset', default='fb15k237', help='Please provide a dataset path')
+parser.add_argument('--workers', default=24, help='Please provide the number of CPUs to run the job')
+parser.add_argument('--model', default='transe', help='Please provide the model to use')
+args = parser.parse_args()
+
+dataset = args.dataset
+dim, epochs = 100, 50
+method = args.model
+workers = args.workers
+
+######
+# Adjust different method
+######
+
+dataset_path = f"data/{dataset}"
+edges_path = f"out/{dataset}/edge"
+model_path = f"out/{dataset}/{args.model}"
+
+
+def get_torchbiggraph_config():
+
+    try_to_make_dir(f"out/{dataset}")
+    try_to_make_dir(edges_path)
+    try_to_make_dir(model_path)
+
+    config = dict(  # noqa
+        # I/O data
+        entity_path=f"{edges_path}/entities",
+        edge_paths=[
+            f"{edges_path}/train",
+            f"{edges_path}/valid",
+            f"{edges_path}/test",
+        ],
+        checkpoint_path=model_path,
+        # Graph structure
+        entities={"all": {"num_partitions": 1}},
+        relations=[
+            {
+                "name": "all_edges",
+                "lhs": "all",
+                "rhs": "all",
+                "operator": op_dict[method]
+            }
+        ],
+        dynamic_relations=True,
+        # Scoring model
+        dimension=dim,
+        global_emb=False,
+        comparator=comp_dict[method],
+        # Training
+        batch_size=50000,
+        num_epochs=epochs,
+        num_uniform_negs=1000,
+        loss_fn="softmax",
+        lr=0.1,
+        # relation_lr=0.01,
+        regularization_coef=1e-3,
+        # Evaluation during training
+        eval_fraction=0,  # to reproduce results, we need to use all training data
+    )
+
+    return config
+
+
+FILENAMES = [
+    f"{dataset_path}/train.txt",
+    f"{dataset_path}/valid.txt",
+    f"{dataset_path}/test.txt"
+]
+
+
+def main():
+    config = parse_config(get_torchbiggraph_config())
+    if workers > 0:
+        config = attr.evolve(config, workers=workers)
+        config = attr.evolve(config, batch_size=10000)
+        config = attr.evolve(config, num_batch_negs=500)
+        config = attr.evolve(config, num_uniform_negs=500)
+    elif torch.cuda.is_available():
+        config = attr.evolve(config, num_gpus=2)
+    else:
+        exit()
+
+    set_logging_verbosity(config.verbose)
+    subprocess_init = SubprocessInitializer()
+    subprocess_init.register(setup_logging, config.verbose)
+    input_edge_paths = [Path(name) for name in FILENAMES]
+    output_train_path, output_valid_path, output_test_path = config.edge_paths
+
+    try:
+        os.mkdir(model_path)
+    except FileExistsError:
+        pass
+
+    log_file_path = f"{model_path}/{args.model}_{datetime.now().strftime('%Y%m%d-%H%M%S')}.log"
+    logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                        level=logging.DEBUG, filename=str(log_file_path), filemode='w')
+
+    logging.info(f"Set Dataset to {dataset}")
+    logging.info(f"Set Model to {args.model}")
+    logging.info(f"Set Dimension to {dim}")
+
+    convert_input_data(
+        config.entities,
+        config.relations,
+        config.entity_path,
+        config.edge_paths,
+        input_edge_paths,
+        TSVEdgelistReader(lhs_col=0, rhs_col=2, rel_col=1),
+        dynamic_relations=config.dynamic_relations,
+    )
+
+    setup_logging()
+
+    relations = [attr.evolve(r, all_negs=True) for r in config.relations]
+    train_config = attr.evolve(config, edge_paths=[output_train_path])
+    valid_config = attr.evolve(config, edge_paths=[output_valid_path], relations=relations,
+                               num_uniform_negs=0, num_batch_negs=0)
+    test_config = attr.evolve(config, edge_paths=[output_test_path], relations=relations,
+                              num_uniform_negs=0, num_batch_negs=0)
+    filter_paths = [output_test_path, output_valid_path, output_train_path]
+    evaluator = FilteredRankingEvaluator(test_config, filter_paths)
+
+    # Do the Training here
+    train(train_config, evaluator=evaluator, subprocess_init=subprocess_init)
+    # validation_config = None, numeric_eval_config = None)
+    do_eval(valid_config, evaluator=evaluator, subprocess_init=subprocess_init)
+    do_eval(test_config, evaluator=evaluator, subprocess_init=subprocess_init)
+
+    '''
+    do_eval(valid_config, 
+            evaluator=FilteredRankingEvaluator(valid_config, filter_paths),
+            subprocess_init=subprocess_init
+           )
+
+    do_eval(test_config, 
+            evaluator=FilteredRankingEvaluator(test_config, filter_paths),
+            subprocess_init=subprocess_init
+           )
+    '''
+
+
+if __name__ == "__main__":
+    main()
--- run.py
+++ run.py
@@ -11,58 +11,63 @@ parser.add_argument('--gpu', default='0', help='Please provide a gpu to assign t
 parser.add_argument('--options', default='', help='Please provide additional instructions if necessary')
 parser.add_argument("--input", type=str, default="data", help="input path")
 parser.add_argument("--output", type=str, default="out", help="output path")
+parser.add_argument("--use_pbg", action="store_true", help="Use PyTorch-BigGraph to compute embedding")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-    model = args.model
-    dataset = args.dataset
-    gpu = args.gpu
-    options = args.options
 
-    if model.startswith("tucker"):
-        if "fb15k237" in dataset:
+    if not args.use_pbg:
+        model = args.model
+        dataset = args.dataset
+        gpu = args.gpu
+
+        if model.startswith("tucker"):
+            if "fb15k237" in dataset:
+                command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
+                          "--num_iterations 500 --batch_size 128 --lr 0.0005 --dr 1.0 --edim 200 --rdim 200 " \
+                          "--input_dropout 0.3 --hidden_dropout1 0.4 --hidden_dropout2 0.5 --label_smoothing 0.1 " \
+                          f"--input {args.input} --output {args.output}"
+            else:
+                command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
+                          "--num_iterations 500 --batch_size 128 --lr 0.003 --dr 0.99 --edim 200 --rdim 200 " \
+                          "--input_dropout 0.2 --hidden_dropout1 0.2 --hidden_dropout2 0.3 --label_smoothing 0.0 " \
+                          f"--input {args.input} --output {args.output}"
+        elif model.startswith("distmult"):
             command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
-                      "--num_iterations 500 --batch_size 128 --lr 0.0005 --dr 1.0 --edim 200 --rdim 200 " \
-                      "--input_dropout 0.3 --hidden_dropout1 0.4 --hidden_dropout2 0.5 --label_smoothing 0.1 " \
+                      "--num_iterations 200 --eval_step 5 --batch_size 128 --lr 0.003 --dr 0.995 " \
+                      "--edim 200 --rdim 200 --input_dropout 0.2 --label_smoothing 0.1 " \
                       f"--input {args.input} --output {args.output}"
+        elif model.startswith("complex"):
+            command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
+                      "--num_iterations 200 --eval_step 5 --batch_size 128 --lr 0.003 --dr 0.995 " \
+                      "--edim 400 --rdim 400 --input_dropout 0.2 --label_smoothing 0.1 " \
+                      f"--input {args.input} --output {args.output}"
+        elif model.startswith("conve"):
+            command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
+                      "--num_iterations 1000 --eval_step 10 --batch_size 128 --lr 0.003 --dr 0.995 " \
+                      "--edim 200 --rdim 200 --input_dropout 0.2 --hidden_dropout1 0.3 --feature_map_dropout 0.2 " \
+                      f"--label_smoothing 0.1 --use_bias --input {args.input} --output {args.output}"
+        elif model.startswith("transe"):
+            command = f"python rotate/create_mapping.py --dataset {args.input}/{dataset} && " \
+                      f"CUDA_VISIBLE_DEVICES={gpu} python -u rotate/run.py --do_train --cuda --do_valid --do_test " \
+                      f"--data_path {args.input}/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                      f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save {args.output}/{dataset}_{model} " \
+                      "--test_batch_size 16"
+        elif model.startswith("rotate"):
+            command = f"python rotate/create_mapping.py --dataset {args.input}/{dataset} && " \
+                      f"CUDA_VISIBLE_DEVICES={gpu} python -u rotate/run.py --do_train --cuda --do_valid --do_test " \
+                      f"--data_path {args.input}/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
+                      f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save {args.output}/{dataset}_{model} " \
+                      "--test_batch_size 16 -de"
         else:
-            command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
-                      "--num_iterations 500 --batch_size 128 --lr 0.003 --dr 0.99 --edim 200 --rdim 200 " \
-                      "--input_dropout 0.2 --hidden_dropout1 0.2 --hidden_dropout2 0.3 --label_smoothing 0.0 " \
-                      f"--input {args.input} --output {args.output}"
-    elif model.startswith("distmult"):
-        command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
-                  "--num_iterations 200 --eval_step 5 --batch_size 128 --lr 0.003 --dr 0.995 " \
-                  "--edim 200 --rdim 200 --input_dropout 0.2 --label_smoothing 0.1 " \
-                  f"--input {args.input} --output {args.output}"
-    elif model.startswith("complex"):
-        command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
-                  "--num_iterations 200 --eval_step 5 --batch_size 128 --lr 0.003 --dr 0.995 " \
-                  "--edim 400 --rdim 400 --input_dropout 0.2 --label_smoothing 0.1 " \
-                  f"--input {args.input} --output {args.output}"
-    elif model.startswith("conve"):
-        command = f"CUDA_VISIBLE_DEVICES={gpu} python tucker/main.py --dataset {dataset} --model {model} " \
-                  "--num_iterations 1000 --eval_step 10 --batch_size 128 --lr 0.003 --dr 0.995 " \
-                  "--edim 200 --rdim 200 --input_dropout 0.2 --hidden_dropout1 0.3 --feature_map_dropout 0.2 " \
-                  f"--label_smoothing 0.1 --use_bias --input {args.input} --output {args.output}"
-    elif model.startswith("transe"):
-        command = f"python rotate/create_mapping.py --dataset {args.input}/{dataset} && " \
-                  f"CUDA_VISIBLE_DEVICES={gpu} python -u rotate/run.py --do_train --cuda --do_valid --do_test " \
-                  f"--data_path {args.input}/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
-                  f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save {args.output}/{dataset}_{model} " \
-                  "--test_batch_size 16"
-    elif model.startswith("rotate"):
-        command = f"python rotate/create_mapping.py --dataset {args.input}/{dataset} && " \
-                  f"CUDA_VISIBLE_DEVICES={gpu} python -u rotate/run.py --do_train --cuda --do_valid --do_test " \
-                  f"--data_path {args.input}/{dataset} --model {model} -n 256 -b 1024 -d 1000 -g 24.0 -a 1.0 -adv " \
-                  f"-lr 0.0001 --max_steps 150000 --valid_steps 5000 -save {args.output}/{dataset}_{model} " \
-                  "--test_batch_size 16 -de"
-    else:
-        print(model, "is not supported")
-        exit()
+            print(model, "is not supported")
+            exit()
 
-    if options == "dry-run":
+    else:
+        command = f"python pbg/main.py --model {args.model} --dataset {args.dataset}"
+
+    if args.options == "dry-run":
         print(command)
     else:
         subprocess.run(command, shell=True)
